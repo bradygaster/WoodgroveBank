da1fd81b9bc1c303c3c8ad016efbc046e873e0af
bradygaster <bradyg@microsoft.com>
2022-02-07 13:03:31 -0800

    removed old project, added support for tye/dapr

--- CustomerATM/Program.cs
+++ CustomerATM/Program.cs
@@ -1,13 +1,8 @@
-using CustomerATM.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
@@ -17,11 +12,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
