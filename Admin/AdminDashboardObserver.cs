3a180e3921a786e715ac49ab2c79683f3972058f
Setup <setup@example.com>
2023-03-31 13:01:23 -0700

    split classes

---
+++ Admin/AdminDashboardObserver.cs
@@ -0,0 +1,23 @@
+﻿using WoodgroveBank.Abstractions;
+
+namespace Admin
+{
+    public class AdminDashboardObserver : IAdminDashboardObserver
+    {
+        private ILogger<AdminDashboardObserver> _logger;
+
+        public AdminDashboardObserver()
+        {
+        }
+
+        public AdminDashboardObserver(ILogger<AdminDashboardObserver> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnCustomerIndexUpdated(Customer customer)
+        {
+            _logger.LogInformation($"{customer.Name} updated");
+        }
+    }
+}
--- Admin/Program.cs
+++ Admin/Program.cs
@@ -1,3 +1,4 @@
+using Admin;
 using WoodgroveBank.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.AddWoodgroveBankSilo(siloBuilder =>
 {
     siloBuilder.ConfigureServices(services =>
     {
+        services.AddSingleton<IAdminDashboardObserver, AdminDashboardObserver>();
         services.AddHostedService<AdminDashboardObserverHost>();
     });
 });
@@ -28,66 +30,3 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
-
-public class AdminDashboardObserver : IAdminDashboardObserver
-{
-    public void OnCustomerIndexUpdated(Customer customer)
-    {
-        Console.WriteLine($"{customer.Name} updated");
-    }
-}
-
-public class AdminDashboardObserverHost : IHostedService
-{
-    private IAdminDashboardObserver _observer;
-    private IClusterClient _clusterClient;
-    private IBankGrain _bankGrain;
-    private IAdminDashboardObserver _observerReference;
-    private ILogger<AdminDashboardObserverHost> _logger;
-    private Timer _delay;
-
-    public AdminDashboardObserverHost(IClusterClient clusterClient, ILogger<AdminDashboardObserverHost> logger)
-    {
-        _observer = new AdminDashboardObserver();
-        _clusterClient = clusterClient;
-        _logger = logger;
-    }
-
-    private async Task Start()
-    {
-        _delay.Dispose();
-
-        _logger.LogInformation("Getting bank grain from cluster");
-        _bankGrain = _clusterClient.GetGrain<IBankGrain>(Guid.Empty);
-
-        _logger.LogInformation("Getting customer list from grain to initialize it.");
-        var customers = await _bankGrain.GetCustomers();
-        _logger.LogInformation($"Got {customers.Length} customers");
-
-        _logger.LogInformation("Creating reference to the local observer instance.");
-        _observerReference = _clusterClient.CreateObjectReference<IAdminDashboardObserver>(_observer);
-        _logger.LogInformation("Created reference to the local observer instance.");
-
-        if (_observerReference == null)
-        {
-            _logger.LogInformation("Created reference IS NULL.");
-        }
-        else
-        {
-            _logger.LogInformation("Subscribing using the observer.");
-            await _bankGrain.Subscribe(_observerReference);
-            _logger.LogInformation("Subscribed using the observer.");
-        }
-    }
-
-    public Task StartAsync(CancellationToken cancellationToken)
-    {
-        _delay = new Timer(async _ => await Start(), null, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(5));
-        return Task.CompletedTask;
-    }
-
-    public async Task StopAsync(CancellationToken cancellationToken)
-    {
-        await _bankGrain.Unsubscribe(_observerReference);
-    }
-}
