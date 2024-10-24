bf34d78daca3e8854ec44b1a968598a3f2d0cefe
Brady Gaster <bradyg@microsoft.com>
2024-10-23 22:18:02 -0700

    NOT WORKING [ping @davidfowl]

---
+++ AppHost/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using Aspire.Hosting.Azure;
+using Azure.Provisioning;
+
+namespace AppHost;
+
+public static class Extensions
+{
+    public static ProvisioningParameter AsProvisioningParameter(this EndpointReference endpointReference, AzureResourceInfrastructure infrastructure, string? parameterName = null)
+    {
+        ArgumentNullException.ThrowIfNull(endpointReference);
+        ArgumentNullException.ThrowIfNull(infrastructure);
+
+        parameterName ??= Infrastructure.NormalizeIdentifierName(((IManifestExpressionProvider)endpointReference).ValueExpression);
+
+        infrastructure.AspireResource.Parameters[parameterName] = endpointReference;
+
+        var parameter = infrastructure.GetParameters().FirstOrDefault(p => p.IdentifierName == parameterName);
+        if (parameter is null)
+        {
+            parameter = new ProvisioningParameter(parameterName, typeof(string));
+            infrastructure.Add(parameter);
+        }
+
+        return parameter;
+    }
+}
